8f85aee622feffe2e119b4bb040551169a95062d
Krzysztof Owsiany <gocom@o2.pl>
2012-11-02 23:39:59 +0100

    PROTOTYPW

--- timerun.js
+++ timerun.js
@@ -1,87 +1,94 @@
-var ctx;
-var width = 100;
-var height = 100;
-var pos=1;
-var left=50;
-var right=50;
-var wsk_size=4;
-var time_step;
-var top_1;
-var top_5;
-function drawLine(x1,y1,x2,y2, color)
-{
-	ctx.strokeStyle = color;
-	ctx.beginPath();
 
-	ctx.moveTo(x1,y1);
-	ctx.lineTo(x2,y2);
-	ctx.stroke();
-}
+var TimeRun = Class.create( {
+	initialize: function (){
+			this.width = 100;
+			this.height = 100;	
+			this.pos=1;
+			this.left=50;
+			this.right=50;
+			this.wsk_size=4;
+			this.time_step;
+			this.top_1;
+			this.top_5;
+			this.ctx=Null;
+			this.time_pos = new Array();
+			window.onload = this.onLoad;
+		},
 
-function drawNumber(x,y,txt,  color)
-{
-	ctx.strokeStyle = color;
-	ctx.font="10px Georgia";
-	ctx.strokeText(txt,x,y);
-}
-
-function drawTime()
-{
-	for (x=0;x<65;x++)
-	{
-		if ((x % 5) == 0)
+		drawLine: function(x1,y1,x2,y2, color)
 		{
-			drawNumber(x*time_step +pos,
-			top_5 - 5,x*time_step+pos, x,"red");
-			drawLine(x*time_step +pos,top_5,x*time_step+pos,height, "black");
+			this.ctx.strokeStyle = color;
+			this.ctx.beginPath();
+
+			this.ctx.moveTo(x1,y1);
+			this.ctx.lineTo(x2,y2);
+			this.ctx.stroke();
+		},
+
+		addTimePos: function(dt,name)
+		{
+			this.time_pos.push({dt:dt, name:name});	
+		},
+
+		drawNumber: function(x,y,txt,  color)
+		{
+			this.ctx.strokeStyle = color;
+			this.ctx.font="10px Georgia";
+			this.ctx.strokeText(txt,x,y);
+		},
+
+		drawTime: function()
+		{
+			for (x=0;x<65;x++)
+			{
+				if ((x % 5) == 0)
+				{
+					this.drawNumber(x*time_step +pos,
+					top_5 - 5,x*time_step+pos, x,"red");
+					this.drawLine(x*time_step +pos,top_5,x*time_step+pos,height, "black");
+				}
+				else
+					this.drawLine(x*time_step+pos,top_1,x*time_step+pos,height, "black");
+			}
+		},
+
+		drawRect: function(x,y,width,height,color)
+		{
+			this.ctx.strokeStyle=color;
+			this.ctx.strokeRect(x,y,width,height);
+		},
+
+		drawFillRect: function(x,y,width,height,color)
+		{
+			this.ctx.fillStyle=color;
+			this.ctx.fillRect(x,y,width,height);
+		},
+
+		redraw: function()
+		{
+			this.drawFillRect(0,0,this.width, this.height, "white");
+			this.drawTime();
+			this.drawFillRect(0,0, this.left, this.height, "rgba(128, 0, 200, 0.5)");
+			this.drawRect(this.left, 0, this.width - this.right - this.left,
+				this.height, "rgba(0, 0, 200, 0.5)");
+			pos--;
+			if ((pos + (time_step*5)) == 0)
+				pos=0;
+		},
+
+		onLoad: function ()
+		{
+			var c=document.getElementById("timeRun");
+			ctx=c.getContext("2d");	
+			width = c.width;
+			height = c.height;		
+			right = left = (width - wsk_size) /2;
+			time_step = width / 60;
+			top_5 = Math.floor(height * 0.6);
+			top_1 = Math.floor(height * 0.8);
+			setInterval(function(){redraw()},1000);	
 		}
-		else
-			drawLine(x*time_step+pos,top_1,x*time_step+pos,height, "black");
-	}
-}
+	});
 
-function drawRect(x,y,width,height,color)
-{
-	ctx.strokeStyle=color;
-	ctx.strokeRect(x,y,width,height);
-}
-
-function drawFillRect(x,y,width,height,color)
-{
-	ctx.fillStyle=color;
-	ctx.fillRect(x,y,width,height);
-}
-
-function redraw()
-{
-	drawFillRect(0,0,width,height, "white");
-	drawTime();
-	drawFillRect(0,0,left,height, "rgba(128, 0, 200, 0.5)");
-	drawRect(left,0,width - right - left,height, "rgba(0, 0, 200, 0.5)");
-	pos--;
-	if ((pos + (time_step*5)) == 0)
-		pos=0;
-}
-
-
-
-
-
-
-
-
-function onLoad()
-{
-	var c=document.getElementById("timeRun");
-	ctx=c.getContext("2d");	
-	width = c.width;
-	height = c.height;		
-	right = left = (width - wsk_size) /2;
-	time_step = width / 60;
-	top_5 = Math.floor(height * 0.6);
-	top_1 = Math.floor(height * 0.8);
-	setInterval(function(){redraw()},1000);
 	
-}
-window.onload = onLoad;
-
+	var tm = new TimeRun(); 
